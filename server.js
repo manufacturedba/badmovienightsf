a5b49e2dc891ddead4510262006ab8e4435233af
Roberto Rodriguez <manufacturedba@gmail.com>
2018-12-12 01:01:20 -0800

    api?

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const request = require('request');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const { MEET_UP_API_KEY } = process.env;
+const MEET_UP_API_ENDPOINT = 'https://api.meetup.com';
+const MEET_UP_GROUP_NAME = 'Bad-Movie-Night-San-Francisco';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+const groupUrlRoot = `${MEET_UP_API_ENDPOINT}/${MEET_UP_GROUP_NAME}`;
+
+function getEvents() {
+  return `${groupUrlRoot}/events`;
+}
+
+function getEventPhotosById(eventId) {
+  return `${groupUrlRoot}/events/${eventId}/photos`;
+}
+
+app.get('/api/events', (req, res) => {
+  const url = getEvents();
+  const qs = {
+    key: MEET_UP_API_KEY,
+  };
+  request.get({ url, qs }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.json(body);
+    }
+  });
 });
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+
+app.get('/api/event/:eventId/photos', (req, res) => {
+  const { eventId } = req.params;
+  const url = getEventPhotosById(eventId);
+  const qs = {
+    key: MEET_UP_API_KEY,
+  };
+  request.get({ url, qs }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.json(body);
+    }
+  });
 });
 if (process.env.NODE_ENV === 'production') {
-  console.log(MEET_UP_API_KEY);
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
