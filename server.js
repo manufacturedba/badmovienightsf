5a551cc1489c387eaf3f06642d10ba2c92251771
Roberto Rodriguez <manufacturedba@gmail.com>
2018-12-12 00:32:24 -0800

    good to go

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+const { MEET_UP_API_KEY } = process.env;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // API calls
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
-app.post("/api/world", (req, res) => {
+app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
+  console.log(MEET_UP_API_KEY);
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
