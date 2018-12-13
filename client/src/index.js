c852f03d47bf265bcc65924cec240bfd59981355
Roberto Rodriguez <manufacturedba@gmail.com>
2018-12-13 01:50:54 -0800

    i don't trust anyone

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
+import { faMeetup } from '@fortawesome/free-brands-svg-icons/faMeetup'
 
-library.add(fab);
+library.add(faFacebook, faGithub, faMeetup);
 ReactDOM.render(<App />, document.getElementById('root'));
